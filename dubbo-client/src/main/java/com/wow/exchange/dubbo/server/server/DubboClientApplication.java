1f4bda3ed4d44a24dab82fe6804d46bd6e0882df
kevin <kevinhan9527@gmail.com>
2024-12-23 00:10:04 +0800

    update dubbo version

--- dubbo-client/src/main/java/com/wow/exchange/dubbo/server/server/DubboClientApplication.java
+++ dubbo-client/src/main/java/com/wow/exchange/dubbo/server/server/DubboClientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DubboServerApplication {
+public class DubboClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DubboServerApplication.class, args);
+        SpringApplication.run(DubboClientApplication.class, args);
     }
 
 }
--- dubbo-server/src/main/java/com/wow/exchange/dubbo/server/DubboServerApplication.java
+++ dubbo-server/src/main/java/com/wow/exchange/dubbo/server/DubboServerApplication.java
@@ -1,8 +1,11 @@
 package com.wow.exchange.dubbo.server;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDubbo
 @SpringBootApplication
 public class DubboServerApplication {
 
